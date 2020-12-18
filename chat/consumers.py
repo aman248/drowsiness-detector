f7c7fac6652fe6b50abe4aff6196bcefc8c0999e
aman248 <baman248@gmail.com>
2020-12-19 04:21:06 +0530

    commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,4 @@
-# chat/consumers.py
-from chat.views import Is_d
+# chat/consumers.pyś
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 import cv2
@@ -30,6 +29,8 @@ class StreamConsumer(AsyncWebsocketConsumer):
         img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.hog_face_detector(gray)
+        if not faces:
+            print('no face')
         for face in faces:
 
             face_landmarks = self.dlib_facelandmark(gray, face)
@@ -54,7 +55,7 @@ class StreamConsumer(AsyncWebsocketConsumer):
             EAR = round(EAR,2)
             print(EAR)
             if EAR<0.26:
-                await self.send('drowsy.....')
+                await self.send('drowsy')
             else:
                 await self.send('checking.....')
     def calculate_EAR(self,eye):
@@ -63,103 +64,3 @@ class StreamConsumer(AsyncWebsocketConsumer):
         C = distance.euclidean(eye[0], eye[3])
         ear_aspect_ratio = (A+B)/(2.0*C)
         return ear_aspect_ratio
-
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-        self.check = True
-        self.is_d = False
-
-        await self.accept()
-        self.t = threading.Thread(target=self.dece)
-        self.t.start()
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        self.check = False
-        self.t.join()
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print(message)
-        a = self.is_d
-        if message == 'start':
-            self.check = True
-            self.dec()
-            print('thread started')
-        elif message == 'stop':
-            self.check = False
-            self.t.join()
-        print(a)
-        await self.send(text_data=json.dumps({
-            'message': 'drowsy' + str(a)
-        }))
-    def calculate_EAR(self,eye):
-        A = distance.euclidean(eye[1], eye[5])
-        B = distance.euclidean(eye[2], eye[4])
-        C = distance.euclidean(eye[0], eye[3])
-        ear_aspect_ratio = (A+B)/(2.0*C)
-        return ear_aspect_ratio
-    async def dec(self):
-        print('goooooooooooooooooo')
-        await self.dece()
-    async def dec2(self):
-        print('dec2 started')
-        await self.send(text_data=json.dumps({
-            'message': 'welcome' + str(i)
-        }))
-
-    def dece(self):
-        print('dece started')
-        cap = cv2.VideoCapture(0)
-        file_ = os.path.join(settings.BASE_DIR,"shape_predictor_68_face_landmarks.dat" )
-        hog_face_detector = dlib.get_frontal_face_detector()
-        dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-        while self.check:
-            _, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            faces = hog_face_detector(gray)
-            for face in faces:
-
-                face_landmarks = dlib_facelandmark(gray, face)
-                leftEye = []
-                rightEye = []
-
-                for n in range(36,42):
-                    x = face_landmarks.part(n).x
-                    y = face_landmarks.part(n).y
-                    leftEye.append((x,y))
-                    next_point = n+1
-                    if n == 41:
-                        next_point = 36
-                    x2 = face_landmarks.part(next_point).x
-                    y2 = face_landmarks.part(next_point).y
-                    cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
-
-                for n in range(42,48):
-                    x = face_landmarks.part(n).x
-                    y = face_landmarks.part(n).y
-                    rightEye.append((x,y))
-                    next_point = n+1
-                    if n == 47:
-                        next_point = 42
-                    x2 = face_landmarks.part(next_point).x
-                    y2 = face_landmarks.part(next_point).y
-                    cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
-
-                left_ear = self.calculate_EAR(leftEye)
-                right_ear = self.calculate_EAR(rightEye)
-
-                EAR = (left_ear+right_ear)/2
-                EAR = round(EAR,2)
-                self.is_d = False
-                if EAR<0.26:
-                    cv2.putText(frame,"DROWSY",(20,100),
-                        cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),4)
-                    cv2.putText(frame,"Are you Sleepy?",(20,400),
-                        cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),4)
-                    self.is_d = True
--- mysite/settings.py
+++ mysite/settings.py
@@ -14,10 +14,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 SECRET_KEY = "y!z^)2-dr-ipiee#cabd#(f@$hfhm-og3e8@fzz)k=h5-_w=p+"
 import os
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 
